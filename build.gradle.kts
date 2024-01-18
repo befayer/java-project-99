198f37999320450e12dcdf8691fe8b6364b0bd9e
Aleksandr Kalinin <a.kalinin@haulmont.com>
2024-01-18 13:05:25 +0400

    Fix Docker config

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+	java
+	application
+	jacoco
+	id("org.springframework.boot") version "3.2.1"
+	id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "hexlet.code"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_20
+}
+
+repositories {
+	mavenCentral()
+}
+
+application {
+	mainClass.set("hexlet.code.AppApplication")
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web:3.2.0")
+	runtimeOnly("com.h2database:h2")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+tasks.bootBuildImage {
+	builder.set("paketobuildpacks/builder-jammy-base:latest")
+}
+
+tasks.jacocoTestReport {
+	reports {
+		xml.required.set(true)
+	}
+}
