1f2af5e202e39e729d02d362fc9902a22c9ba71e
Aleksandr Kalinin <a.kalinin@haulmont.com>
2024-01-18 14:13:50 +0400

    Fix Docker 20000%

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-web:3.2.0")
+	implementation("org.springframework.boot:spring-boot-starter-web")
 	runtimeOnly("com.h2database:h2")
 }
 
