35de29809864a87f9fc9d85d427834e28cf160e0
Aleksandr Kalinin <a.kalinin@haulmont.com>
2024-01-18 13:58:37 +0400

    Fix Docker config

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ plugins {
 	java
 	application
 	jacoco
-	id("org.springframework.boot") version "3.2.1"
-	id("io.spring.dependency-management") version "1.1.4"
+	id("org.springframework.boot") version "3.1.5"
+	id("io.spring.dependency-management") version "1.1.3"
 }
 
 group = "hexlet.code"
